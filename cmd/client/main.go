86eb7ac4b95fbf267dcce0541c559b16874b6cd7
Gustavo da Silva Oliveira <silva.gustavo98@gmail.com>
2025-09-02 00:57:08 -0300

    feat: lipgloss adicionado

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,22 +2,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"runtime"
-	"strings"
-	"time"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/term"
 )
 
 func main() {
+
 	// A lógica para abrir o terminal é diferente por sistema operacional.
 	// O nosso "client" vai executar um novo terminal e passar a si mesmo como argumento.
 	// Este é o método "double-exec".
 	if len(os.Args) > 1 && os.Args[1] == "in-terminal" {
-		fmt.Println(`
+		notes := `
 
 
 :::'###::::'########:::::'##::: ##::'#######::'########:'########::'######::
@@ -31,12 +34,41 @@ func main() {
 		
 
 		
-		`)
+		`
 
-		fmt.Println("\n\n\nEscreva sua anotação abaixo!")
-		annotation := scanner()
-		writeTxt(annotation)
-		time.Sleep(1 * time.Second)
+		/*
+			fmt.Println("\n\n\nEscreva sua anotação abaixo!")
+			annotation := scanner()
+			writeTxt(annotation)
+			time.Sleep(1 * time.Second)
+			return*/
+
+		termWid, termHeight, err := term.GetSize(os.Stdout.Fd())
+		if err != nil {
+			fmt.Println(nil)
+			return
+		}
+
+		var style = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#FAFAFA")).
+			Background(lipgloss.Color("#7D56F4")).
+			Height(termHeight / 2)
+		// Centraliza o conteúdo
+		output := lipgloss.Place(
+			termWid,
+			termHeight/2,
+			lipgloss.Center,
+			lipgloss.Center,
+			style.Render(notes),
+		)
+
+		fmt.Println(output)
+		p := tea.NewProgram(initialModel())
+
+		if _, err := p.Run(); err != nil {
+			log.Fatal((err))
+		}
 		return
 	}
 
@@ -71,16 +103,17 @@ func main() {
 	}
 }
 
-func scanner() string {
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		annotation_str, _ := reader.ReadString('\n')
-		annotation_str = strings.TrimSpace(annotation_str)
+/*
+	func scanner() string {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			annotation_str, _ := reader.ReadString('\n')
+			annotation_str = strings.TrimSpace(annotation_str)
 
-		return annotation_str
+			return annotation_str
+		}
 	}
-}
-
+*/
 func writeTxt(msg string) {
 	// Abre o arquivo para anexar, cria se não existir
 	f, err := os.OpenFile("annotations.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -95,4 +128,84 @@ func writeTxt(msg string) {
 	}
 
 	log.Println("Arquivo escrito com sucesso!")
+}
+
+type errMsg error
+
+type model struct {
+	textarea textarea.Model
+	err      error
+}
+
+func initialModel() model {
+	ti := textarea.New()
+	ti.Placeholder = "Once upon a time..."
+	ti.Focus()
+
+	return model{
+		textarea: ti,
+		err:      nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc:
+			if m.textarea.Focused() {
+				m.textarea.Blur()
+			}
+		case tea.KeyCtrlS:
+			//fmt.Println(m.textarea.Value())
+			writeTxt(m.textarea.Value())
+			return m, tea.Quit
+		default:
+			if !m.textarea.Focused() {
+				cmd = m.textarea.Focus()
+				cmds = append(cmds, cmd)
+			}
+		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	termWid, termHeight, err := term.GetSize(os.Stdout.Fd())
+	if err != nil {
+		fmt.Println(nil)
+		return ""
+	}
+	var textStyle = lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("63")).
+		Padding(1, 1).
+		Width((termWid / 2) * 9 / 10)
+
+	content := fmt.Sprintf(
+		"Tellme a story. \n\n%s \n\n%s",
+		m.textarea.View(),
+		"(Ctrl+s to Save and Quit)",
+	) + "\n\n"
+	output := lipgloss.Place(
+		termWid/2,
+		termHeight/2,
+		lipgloss.Center,
+		lipgloss.Center,
+		textStyle.Render(content))
+
+	return output
 }
